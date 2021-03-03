5ab00984994ac66a4c9c91028d5270d57f3362f5
Samohval Sergey <sergeysamohval17@gmail.com>
2021-03-03 18:59:00 +0300

    ToDo

--- ToDo.py
+++ ToDo.py
@@ -1,14 +1,53 @@
-todos = []
-menu = ''
+import time
+
+#import fileinput
+#import sys
+
+#def replaceAll(file,searchExp,replaceExp):
+#      if searchExp in line:
+#            line = line.replace(searchExp,replaceExp)
+#        sys.stdout.write(line)
+
+menu = "1. Показать список дел \n" \
+       "2. Добавить задачу \n" \
+       "3. Удалить \n" \
+       "0. Выход"
+def print_menu():
+	print(menu)
 
 def print_todo():
-	pass	
+    with open('todo.txt', 'r', encoding='utf-8') as todo:
+    	print(todo.read()) # как сделать чтобы выводилось сообшение при пустом списке(файл пуст)   	
 
-def add_todo(text):
-	pass
 
-def change_todo(index, text):
-	pass	
+def add_todo():
+	a = input('Введите новую задачу: ')
+	with open('todo.txt', 'a', encoding='utf-8') as todo:
+		todo.write(a + '\n')
 
-def delete_todo(index):
-	pass
+def delete_todo():
+	with open('todo.txt', 'w', encoding='utf-8') as todo:
+		todo.write('СПИСОК ЗАДАЧ ПУСТ')
+		print('Список задач очищен')# как удалить необходимую нам строчку (как произвести переход на необходимую строчку)
+									# как выбрать необходимую нам внесенную задачу и произвести ее замену
+print('Приветствую с списке задач!')
+time.sleep(1)
+print('Вы можете выбрать следующее:') 
+time.sleep(1)
+print(menu)
+time.sleep(2) 
+
+while True:
+    command = input("Введите команду:  " )
+    if command not in ("1","2","3","4","0"):
+            print("Введите необходимую Вам команду!")
+            time.sleep(1)
+            print_menu()
+    elif command == "1":
+            print_todo()
+    elif command == "2":
+            add_todo()
+    elif command == '3':
+           delete_todo()
+    elif command =='0':
+       	break
